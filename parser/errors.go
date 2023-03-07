635ff3ff89d0b9aec16e0eaca09377d9a7a722e6
Clement <clement.jean@epitech.eu>
2023-03-07 10:15:45 +0800

    Add errors

---
+++ parser/errors.go
@@ -0,0 +1,9 @@
+package parser
+
+const (
+	errorUnexpectedPeek        = "expected [%s], got %s"
+	errorUnexpectedOptionValue = "expected value of type %s for option %s"
+	errorUnknownKeyword        = "unknown keyword: %s"
+	errorUnknownOption         = "unknown option: %s"
+	errorDeprecatedOption      = "option %s is deprecated"
+)
