f1535b08400ab6f7d9ab866da501e6e09245dd5b
Clement <clement.jean@epitech.eu>
2023-03-07 10:12:08 +0800

    make runCheck functions more generic

--- parser/parser_syntax_test.go
+++ parser/parser_syntax_test.go
@@ -1,15 +1,18 @@
 package parser
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/Clement-Jean/protein/lexer"
 )
 
-func runSyntaxCheck(t *testing.T, expected *string, tokens []lexer.Token) {
-	l := &FakeLexer{tokens: tokens}
-	p := New(l)
-	d := p.Parse()
+func runSyntaxCheck(t *testing.T, expected *string, tokens []lexer.Token) string {
+	d, err := runCheck(t, tokens)
+
+	if len(err) != 0 {
+		return err
+	}
 
 	if expected != d.Syntax && (d.Syntax == nil || expected == nil || *d.Syntax != *expected) {
 		if d.Syntax == nil {
@@ -20,54 +23,79 @@ func runSyntaxCheck(t *testing.T, expected *string, tokens []lexer.Token) {
 
 		t.Fatalf("syntax wrong. expected='%s', got='%s'", *expected, *d.Syntax)
 	}
+
+	return ""
 }
 
 func TestParseSyntaxProto3(t *testing.T) {
 	// syntax = "proto3";
 	expected := "proto3"
-	runSyntaxCheck(t, &expected, []lexer.Token{
+	err := runSyntaxCheck(t, &expected, []lexer.Token{
 		{Type: lexer.TokenIdentifier, Literal: "syntax", Position: lexer.Position{}},
 		{Type: lexer.TokenEqual, Literal: "=", Position: lexer.Position{}},
 		{Type: lexer.TokenStr, Literal: "\"proto3\"", Position: lexer.Position{}},
 		{Type: lexer.TokenSemicolon, Literal: ";", Position: lexer.Position{}},
 	})
+
+	if len(err) != 0 {
+		t.Fatalf("unexpected error: %s", err)
+	}
 }
 
 func TestParseSyntaxProto2(t *testing.T) {
 	// syntax = 'proto2';
 	expected := "proto2"
-	runSyntaxCheck(t, &expected, []lexer.Token{
+	err := runSyntaxCheck(t, &expected, []lexer.Token{
 		{Type: lexer.TokenIdentifier, Literal: "syntax", Position: lexer.Position{}},
 		{Type: lexer.TokenEqual, Literal: "=", Position: lexer.Position{}},
 		{Type: lexer.TokenStr, Literal: "'proto2'", Position: lexer.Position{}},
 		{Type: lexer.TokenSemicolon, Literal: ";", Position: lexer.Position{}},
 	})
+
+	if len(err) != 0 {
+		t.Fatalf("unexpected error: %s", err)
+	}
 }
 
 func TestParseSyntaxExpectedEqual(t *testing.T) {
 	// syntax 'proto2';
-	runSyntaxCheck(t, nil, []lexer.Token{
+	err := runSyntaxCheck(t, nil, []lexer.Token{
 		{Type: lexer.TokenIdentifier, Literal: "syntax", Position: lexer.Position{}},
 		{Type: lexer.TokenStr, Literal: "'proto2'", Position: lexer.Position{}},
 		{Type: lexer.TokenSemicolon, Literal: ";", Position: lexer.Position{}},
 	})
+
+	expectedErr := fmt.Sprintf(errorUnexpectedPeek, "=", "String")
+	if err != expectedErr {
+		t.Fatalf("error wrong. expected='%s', got='%s'", expectedErr, err)
+	}
 }
 
 func TestParseSyntaxExpectedStr(t *testing.T) {
 	// syntax = proto2;
-	runSyntaxCheck(t, nil, []lexer.Token{
+	err := runSyntaxCheck(t, nil, []lexer.Token{
 		{Type: lexer.TokenIdentifier, Literal: "syntax", Position: lexer.Position{}},
 		{Type: lexer.TokenEqual, Literal: "=", Position: lexer.Position{}},
 		{Type: lexer.TokenIdentifier, Literal: "proto2", Position: lexer.Position{}},
 		{Type: lexer.TokenSemicolon, Literal: ";", Position: lexer.Position{}},
 	})
+
+	expectedErr := fmt.Sprintf(errorUnexpectedPeek, "String", "Identifier")
+	if err != expectedErr {
+		t.Fatalf("error wrong. expected='%s', got='%s'", expectedErr, err)
+	}
 }
 
 func TestParseSyntaxExpectedSemicolon(t *testing.T) {
 	// syntax = 'proto2'
-	runSyntaxCheck(t, nil, []lexer.Token{
+	err := runSyntaxCheck(t, nil, []lexer.Token{
 		{Type: lexer.TokenIdentifier, Literal: "syntax", Position: lexer.Position{}},
 		{Type: lexer.TokenEqual, Literal: "=", Position: lexer.Position{}},
 		{Type: lexer.TokenStr, Literal: "'proto2'", Position: lexer.Position{}},
 	})
+
+	expectedErr := fmt.Sprintf(errorUnexpectedPeek, ";", "EOF")
+	if err != expectedErr {
+		t.Fatalf("error wrong. expected='%s', got='%s'", expectedErr, err)
+	}
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,7 +1,12 @@
 package parser
 
 import (
+	"io"
+	"os"
+	"testing"
+
 	"github.com/Clement-Jean/protein/lexer"
+	"google.golang.org/protobuf/types/descriptorpb"
 )
 
 type FakeLexer struct {
@@ -17,4 +22,29 @@ func (l *FakeLexer) NextToken() lexer.Token {
 	token := l.tokens[l.i]
 	l.i++
 	return token
+}
+
+func runCheck(t *testing.T, tokens []lexer.Token) (*descriptorpb.FileDescriptorProto, string) {
+	r, w, err := os.Pipe()
+
+	if err != nil {
+		t.Fatalf("couldn't create pipe")
+	}
+
+	oldStderr := os.Stderr
+	os.Stderr = w
+	defer func() { os.Stderr = oldStderr }()
+
+	l := &FakeLexer{tokens: tokens}
+	p := New(l)
+	d := p.Parse()
+
+	w.Close()
+	errors, err := io.ReadAll(r)
+
+	if err != nil {
+		t.Fatalf("couldn't read stderr")
+	}
+
+	return &d, string(errors)
 }
