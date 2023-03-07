efba14c4de3613b158e54ac96752a74379826464
Clement <clement.jean@epitech.eu>
2023-03-07 10:21:09 +0800

    Accept more generic, error function and parseFuncs

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Clement-Jean/protein
 
 go 1.19
 
-require google.golang.org/protobuf v1.28.1 // indirect
+require (
+	golang.org/x/exp v0.0.0-20230304125523-9ff063c70017 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
--- parser/impl.go
+++ parser/impl.go
@@ -1,7 +1,12 @@
 package parser
 
 import (
+	"fmt"
+	"os"
+	"strings"
+
 	"github.com/Clement-Jean/protein/lexer"
+	"golang.org/x/exp/slices"
 	pb "google.golang.org/protobuf/types/descriptorpb"
 )
 
@@ -25,17 +30,40 @@ func (p *Impl) nextToken() {
 	p.peekToken = p.l.NextToken()
 }
 
-// acceptPeek returns true and advance token
-// if the type t is equal to the peekToken.Type
-// else it returns false
-func (p *Impl) acceptPeek(t lexer.TokenType) bool {
-	if p.peekToken.Type != t {
-		//TODO: add error and update documentation
+func (p *Impl) accept(original lexer.TokenType, expected ...lexer.TokenType) bool {
+	if !slices.Contains(expected, original) {
+		p.error(fmt.Sprintf(
+			errorUnexpectedPeek,
+			strings.Trim(fmt.Sprint(expected), "[]"),
+			original.String(),
+		))
 		return false
 	}
 
 	p.nextToken()
 	return true
+}
+
+// acceptPeek returns true and advance token
+// if the type t is equal to the peekToken.Type
+// else it returns false
+func (p *Impl) acceptPeek(tt ...lexer.TokenType) bool {
+	return p.accept(p.peekToken.Type, tt...)
+}
+
+func (p *Impl) error(msg string) {
+	fmt.Fprint(os.Stderr, msg)
+}
+
+var parseFuncs = map[string]func(p *Impl, d *pb.FileDescriptorProto){
+	"syntax":  func(p *Impl, d *pb.FileDescriptorProto) { d.Syntax = p.parseSyntax() },
+	"package": func(p *Impl, d *pb.FileDescriptorProto) { d.Package = p.parsePackage() },
+	"import": func(p *Impl, d *pb.FileDescriptorProto) {
+		dep := p.parseImport()
+		if len(dep) != 0 {
+			d.Dependency = append(d.Dependency, dep)
+		}
+	},
 }
 
 // Parse populates a FileDescriptorProto
@@ -44,12 +72,13 @@ func (p *Impl) Parse() pb.FileDescriptorProto {
 
 	for p.curToken.Type != lexer.EOF {
 		if p.curToken.Type == lexer.TokenIdentifier {
-			switch p.curToken.Literal {
-			case "syntax":
-				d.Syntax = p.parseSyntax()
-			default:
+			fn, ok := parseFuncs[p.curToken.Literal]
+			if !ok {
+				//TODO: recovering
+				//p.error(fmt.Sprintf(errorUnknownKeyword, p.peekToken.Literal))
 				break
 			}
+			fn(p, &d)
 		}
 		p.nextToken()
 	}
