9c3b23dbacb7d5e635a0a73b1ca1994978c71160
Clement <clement.jean@epitech.eu>
2023-03-09 13:23:56 +0800

    Removing error to make more generic one

--- parser/errors.go
+++ parser/errors.go
@@ -1,9 +1,8 @@
 package parser
 
 const (
-	errorUnexpectedPeek        = "expected [%s], got %s"
-	errorUnexpectedOptionValue = "expected value of type %s for option %s"
-	errorUnknownKeyword        = "unknown keyword: %s"
-	errorUnknownOption         = "unknown option: %s"
-	errorDeprecatedOption      = "option %s is deprecated"
+	errorUnexpected       = "expected [%s], got %s"
+	errorUnknownKeyword   = "unknown keyword: %s"
+	errorUnknownOption    = "unknown option: %s"
+	errorDeprecatedOption = "option %s is deprecated"
 )
