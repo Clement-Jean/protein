585ac723a01d3554164c1b7cfba922eef0326d15
Clement <clement.jean@epitech.eu>
2024-05-30 10:44:40 +0800

    feat(parser): add stack to parser for implementing state machine

--- parser/parser.go
+++ parser/parser.go
@@ -5,17 +5,49 @@ import "github.com/Clement-Jean/protein/lexer"
 type Parser struct {
 	toks    *lexer.TokenizedBuffer
 	tree    ParseTree
+	stack   []stateStackEntry
 	errs    []error
 	currTok int
 }
 
 func New(toks *lexer.TokenizedBuffer) *Parser {
 	return &Parser{
-		toks: toks,
-		tree: make([]Node, 0, len(toks.TokenInfos)),
+		toks:  toks,
+		tree:  make([]Node, 0, len(toks.TokenInfos)),
+		stack: make([]stateStackEntry, 0, 31),
 	}
 }
 
+func (p *Parser) pushState(st state) {
+	p.stack = append(p.stack, stateStackEntry{
+		st:           st,
+		tokIdx:       p.currTok,
+		subtreeStart: int32(len(p.tree) - 1),
+	})
+}
+
+func (p *Parser) popState() stateStackEntry {
+	state := p.stack[len(p.stack)-1]
+	p.stack = p.stack[:len(p.stack)-1]
+	return state
+}
+
+func (p *Parser) topState() stateStackEntry {
+	return p.stack[len(p.stack)-1]
+}
+
+func (p *Parser) parseTopLevel() {
+	p.popState()
+}
+
 func (p *Parser) Parse() (ParseTree, []error) {
+	p.pushState(stateTopLevel)
+
+	for len(p.stack) != 0 && p.currTok < len(p.toks.TokenInfos) {
+		switch p.topState().st {
+		case stateTopLevel:
+			p.parseTopLevel()
+		}
+	}
 	return p.tree, p.errs
 }
---
+++ parser/state.go
@@ -0,0 +1,24 @@
+package parser
+
+import "fmt"
+
+//go:generate stringer -type=state
+type state uint8
+
+const (
+	stateTopLevel state = iota
+)
+
+type stateStackEntry struct {
+	st           state
+	hasError     bool
+	tokIdx       int
+	subtreeStart int32
+}
+
+func (s stateStackEntry) String() string {
+	return fmt.Sprintf(
+		"{st: %s, hasError: %t, tokIdx: %d, subtreeStart: %d}",
+		s.st, s.hasError, s.tokIdx, s.subtreeStart,
+	)
+}
