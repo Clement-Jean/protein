78de145e575fe1dc1c095af57ee223e02abd55bb
Clement <clement.jean@epitech.eu>
2025-02-21 15:19:33 +0800

    test: add test for packages

--- typecheck/package_test.go
+++ typecheck/package_test.go
@@ -1,3 +1,57 @@
 package typecheck_test
 
-// TODO: multiple package definitions
+import (
+	"testing"
+
+	"github.com/Clement-Jean/protein/typecheck"
+)
+
+type packageTestCase struct {
+	name     string
+	contents []testFile
+	errors   []error
+}
+
+func TestPackage(t *testing.T) {
+	tests := []importTestCase{
+		{
+			name: "trivial package",
+			contents: []testFile{
+				{"a.proto", "package a.b;"},
+			},
+		},
+		{
+			name: "redefined package",
+			contents: []testFile{
+				{"a.proto", "package a.b; package b.a;"},
+			},
+			errors: []error{
+				&typecheck.PackageMultipleDefError{File: "a.proto"},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		units := createUnits(t, test.contents)
+
+		t.Run(test.name, func(t *testing.T) {
+			l := typecheck.New(
+				units,
+				typecheck.WithSourceCreator(fakeSourceCreator(test.contents, test.unknown)),
+				typecheck.WithFileCheck(fakeFileCheck(test.contents, test.unknown)),
+			)
+			errs := l.Check()
+
+			if len(errs) != len(test.errors) {
+				t.Fatalf("expected %d errors, got %d: %v", len(test.errors), len(errs), errs)
+			}
+
+			for i, err := range errs {
+				if err.Error() != test.errors[i].Error() {
+					t.Fatalf("expected %q, got %q", test.errors[i].Error(), err.Error())
+				}
+			}
+		})
+	}
+
+}
